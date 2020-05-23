a8804cf82f24a4378830dddb3a12fa16f8de15d8
itsalexfirst <itsalex@yandex.ru>
2020-05-23 21:39:37 +0300

    Slim gem, rspec g config

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module Qna
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+                       request_specs: false,
+                       controller_specs: true,
+                       view_specs: false,
+                       routing_specs: false,
+                       helper_specs: false
+    end
   end
 end
