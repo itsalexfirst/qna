176255438b4b828cda895df9eef862a2d0fe8547
itsalexfirst <itsalex@yandex.ru>
2020-06-04 23:24:05 +0300

    fixed User#author_of? spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,30 +7,18 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
-  describe 'method .author_of?' do
+  describe '#author_of?' do
     let!(:author) { create(:user) }
-    let!(:user) { create(:user) }
 
     let!(:authors_question) { create(:question, author: author) }
-    let!(:authors_answer) { create(:answer, question: authors_question, author: author) }
+    let!(:users_question) { create(:question) }
 
-    let!(:users_question) { create(:question, author: user) }
-    let!(:users_answer) { create(:answer, question: users_question, author: user) }
-
-    it 'author of own question' do
+    it 'user is author of own question' do
       expect(author).to be_author_of(authors_question)
-    end
-
-    it 'author of own answer' do
-      expect(author).to be_author_of(authors_answer)
     end
 
     it 'user is not author of question' do
       expect(author).to_not be_author_of(users_question)
-    end
-
-    it 'user is not author of answer' do
-      expect(author).to_not be_author_of(users_answer)
     end
   end
 end
