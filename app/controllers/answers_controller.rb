5759560e8f8f516078c80010b7b7dae10ce90533
itsalexfirst <itsalex@yandex.ru>
2020-06-08 19:24:24 +0300

    User can attach many files to answer spec

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -30,10 +30,10 @@ class AnswersController < ApplicationController
   end
 
   def load_answer
-    @answer = Answer.find(params[:id])
+    @answer = Answer.with_attached_files.find(params[:id])
   end
 
   def answer_params
-    params.require(:answer).permit(:body)
+    params.require(:answer).permit(:body, files: [])
   end
 end
--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -25,6 +25,15 @@ feature 'User can create answer', %q{
       expect(page).to have_content 'test text'
     end
 
+    scenario 'answer a question with attached file' do
+      fill_in 'Body', with: 'test text'
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Answer'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
+
     scenario 'asks a question with errors' do
       click_on 'Answer'
       expect(page).to have_content "Body can't be blank"
