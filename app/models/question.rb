27749cb6c08fbd0022b0af335df58524faa85354
itsalexfirst <itsalex@yandex.ru>
2020-06-08 18:37:01 +0300

    User can attach many files to question spec

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   has_many :answers, -> { order('best DESC') }, dependent: :destroy
   belongs_to :author, class_name: 'User'
 
-  has_one_attached :file
+  has_many_attached :files
 
   validates :title, :body, presence: true
 
--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -30,10 +30,11 @@ feature 'User can create question', %q{
     scenario 'asks a question with attached file' do
       fill_in 'Title', with: 'Test question'
       fill_in 'Body', with: 'test text'
-      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
       click_on 'Ask'
 
       expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
     end
 
     scenario 'asks a question with errors' do
