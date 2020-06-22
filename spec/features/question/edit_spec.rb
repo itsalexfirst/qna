9fd646535e3f4c02dcee75ace8875110b1d9be19
itsalexfirst <itsalex@yandex.ru>
2020-06-23 02:02:58 +0300

    add link when edit quetion spec

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -10,6 +10,7 @@ feature 'User can edit his question', %q{
   given(:not_author) { create(:user) }
   given!(:question) { create(:question, author: user) }
   given!(:not_user_question) { create(:question) }
+  given(:test_url) { 'https://yandex.ru' }
 
   describe 'Authenticated user', js: true do
     scenario 'edits his question' do
@@ -39,6 +40,21 @@ feature 'User can edit his question', %q{
 
       expect(page).to have_link 'rails_helper.rb'
       expect(page).to have_link 'spec_helper.rb'
+    end
+
+    scenario 'add link while edits his question' do
+      sign_in user
+      visit root_path
+
+      within "#question-#{question.id}" do
+        click_on 'Edit Question'
+        click_on 'add link'
+        fill_in 'Link name', with: 'Test link'
+        fill_in 'Url', with: test_url
+        click_on 'Save'
+      end
+
+      expect(page).to have_link 'Test link', href: test_url
     end
 
     scenario 'edits his question with errors' do
