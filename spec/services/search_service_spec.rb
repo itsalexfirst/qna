e22500950a86363854416ed3566797cd15f25200
itsalexfirst <itsalex@yandex.ru>
2020-08-21 18:06:27 +0300

    fix conversation

--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -3,23 +3,22 @@ require 'rails_helper'
 RSpec.describe SearchService do
   let(:resources_list) { %w[Question Answer Comment User] }
 
-  let(:search_params) { { keywords: 'myContent', resource: resources_list } }
+  let(:search_params) { { keywords: 'keyword', resource: resources_list } }
 
   it 'search in all resource' do
-    resource = search_params[:resource] || %w[Question Answer Comment User]
     keywords = ThinkingSphinx::Query.escape(search_params[:keywords])
 
-    expect(ThinkingSphinx).to receive(:search).with(keywords, :classes => resource.map { |x| x.classify.constantize }).and_call_original
+    expect(ThinkingSphinx).to receive(:search).with(keywords, :classes => [Question, Answer, Comment, User]).and_call_original
     SearchService.call(search_params)
   end
 
   it 'search in one resource' do
     resources_list.each do |res|
-      resource = [res]
-      search_params = { keywords: 'myContent', resource: resource }
+      search_params = { keywords: 'keyword', resource: [res] }
+      resource = res.classify.constantize
       keywords = ThinkingSphinx::Query.escape(search_params[:keywords])
 
-      expect(ThinkingSphinx).to receive(:search).with(keywords, :classes => resource.map { |x| x.classify.constantize }).and_call_original
+      expect(ThinkingSphinx).to receive(:search).with(keywords, :classes => [resource]).and_call_original
       SearchService.call(search_params)
     end
   end
