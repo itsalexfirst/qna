30bf0665d2b7e56023f172c0704527f0e3d40ce8
itsalexfirst <itsalex@yandex.ru>
2020-08-23 21:58:58 +0300

    application mailer default from credentials

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: Rails.application.credentials[:smtp][:user]
+  default from: Rails.application.credentials[Rails.env.to_sym][:smtp][:user]
   layout 'mailer'
 end
