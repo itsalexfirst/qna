af408284de8c45c1203c9bf6deb5b62a3555bcd4
itsalexfirst <itsalex@yandex.ru>
2020-08-27 02:36:11 +0300

    caching

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, touch: true
 
   validates :body, presence: true
 end
